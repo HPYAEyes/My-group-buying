6196c5ea4c8f342a6544486bf2efa8e94e796567
HPYAEyes <32727441+HPYAEyes@users.noreply.github.com>
2019-03-06 21:02:53 +0800

    initial commit

---
+++ components/public/header/geo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location">杭州市</i>
+    <nuxt-link class="change-city" to="/changeCity">切换城市</nuxt-link>
+    [上海 南京]
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="css">
+
+</style>
+
